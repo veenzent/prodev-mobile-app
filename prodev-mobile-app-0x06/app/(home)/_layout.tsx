3a4700c384c363249100964d87c3d16093fcec5b
veenzent <odumevincent19@gmail.com>
2026-01-21 17:07:55 +0100

    0x06 update

--- prodev-mobile-app-0x06/app/(home)/_layout.tsx
+++ prodev-mobile-app-0x06/app/(home)/_layout.tsx
@@ -28,7 +28,6 @@ const HomeRootLayout = () => {
       }} />
         <Tabs.Screen name="profile" options={{
         title: 'Profile',
-        // headerShown: true,
         tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color="black" />
       }} />
     </Tabs>
